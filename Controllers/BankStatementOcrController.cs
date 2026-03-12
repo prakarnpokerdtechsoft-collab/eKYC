f795ecba8e7d091d09792f660fe2b1edf0633174
TSH <prakarn.techsoftholding@outlook.com>
2026-03-12 16:38:35 +0700

    New Api Statemant-Bank

---
+++ Controllers/BankStatementOcrController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/ocr/bank-statement")]
+public class BankStatementOcrController : ControllerBase
+{
+    private readonly DocumentPipelineService _service;
+
+    public BankStatementOcrController(DocumentPipelineService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetResult(string id)
+    {
+        var result = await _service.GetBankStatementResult(id);
+        return Ok(result);
+    }
+}
